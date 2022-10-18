21d0e0b42158e217a7189a68d10c8bc6914263ce
psmever <psmever@gmail.com>
2022-10-18 13:41:04 +0900

    alias add.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,19 @@
+const CracoAlias = require('craco-alias');
+
 module.exports = {
-    style: {
-        postcssOptions: {
-            plugins: [require("tailwindcss"), require("autoprefixer")],
-        },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: './tsconfig.paths.json',
+        baseUrl: './src',
+      },
     },
+  ],
+  style: {
+    postcssOptions: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '@Style/global.css';
 
 function App() {
   return (
