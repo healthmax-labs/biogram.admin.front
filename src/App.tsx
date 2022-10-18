757c4c664165d1ecb6e1f25090f3fc89e85ef258
psmever <psmever@gmail.com>
2022-10-18 13:00:29 +0900

    eslint, prettier add.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-      <h1 className="text-9xl font-bold underline">
-        Hello world!
-      </h1>
+    <h1 className="text-9xl font-bold underline">
+      Hello world!
+    </h1>
 
   );
 }
