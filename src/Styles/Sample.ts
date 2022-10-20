a7bd12a8031a59a60b242459dcb19f26ecde8aa3
psm <psm@healthmax.co.kr>
2022-10-20 14:19:06 +0900

    twin.macro, emotion 추가.

---
+++ src/Styles/Sample.ts
@@ -0,0 +1,5 @@
+import tw from 'twin.macro'
+
+export const LoginContainer = tw.section`relative flex min-h-screen flex-col justify-center overflow-hidden`
+
+export const LoginWapper = tw.section`m-auto w-full rounded border-t border-purple-600 bg-white p-6 shadow-lg lg:max-w-md`
