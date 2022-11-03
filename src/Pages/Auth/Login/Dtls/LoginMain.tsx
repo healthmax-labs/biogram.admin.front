90b65420a872327fd5d4d447a4a752484ec0f0a4
psm <psm@healthmax.co.kr>
2022-11-03 17:52:30 +0900

    로그인 처리.

--- src/Pages/Auth/Login/Dtls/LoginMain.tsx
+++ src/Pages/Auth/Login/Dtls/LoginMain.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LoginForm from './LoginForm'
+import { useAuth } from '@Hook/index'
+import { useNavigate } from 'react-router-dom'
 
 export default function LoginMain() {
+    const navigate = useNavigate()
+    const { handleLoginCheck } = useAuth()
+
+    useEffect(() => {
+        const funcCheckLogin = async () => {
+            const task = handleLoginCheck()
+            if (task) {
+                navigate({
+                    pathname:
+                        process.env.PUBLIC_URL + `/manage/member/member-list`,
+                })
+            }
+        }
+
+        funcCheckLogin().then()
+    }, [])
     return <LoginForm />
 }
