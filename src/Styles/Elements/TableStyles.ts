88dd4aced64e465a58326f5e7995af10fa55604e
psm <psm@healthmax.co.kr>
2022-11-25 14:32:30 +0900

    테이블 높이 변경.

--- src/Styles/Elements/TableStyles.ts
+++ src/Styles/Elements/TableStyles.ts
@@ -8,10 +8,10 @@ export const TableStyle = {
     HeaderRow: tw.tr`w-full flex bg-m-dip-blue items-center`,
     HeaderCheckbox: tw.th`p-4 w-1/12 px-1 align-middle py-1 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap text-center text-white border-gray-100`,
     HeaderCell: tw.th`p-4 w-1/4 px-3 align-middle py-1 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap text-center text-white border-gray-100`,
-    TableBody: tw.tbody`w-full h-[69vh] bg-gray-100 flex flex-col text-center items-center justify-between overflow-y-scroll`,
+    TableBody: tw.tbody`w-full max-h-[69vh] bg-gray-100 flex flex-col text-center items-center justify-between overflow-y-scroll`,
     TableBodyRow: styled.tr(({ BgState }: { BgState: boolean }) => [
         BgState
-            ? tw`flex w-full bg-white h-9 items-center cursor-pointer hover:bg-green-200`
+            ? tw`flex w-full bg-white max-h-9 items-center cursor-pointer hover:bg-green-200`
             : tw`flex w-full h-9 items-center cursor-pointer hover:bg-green-200`,
     ]),
     TableBodyCell: tw.td`flex items-center justify-center w-1/4 h-10 align-middle text-xs uppercase border-l-0 border-r-0 whitespace-nowrap text-center text-gray-500 border-gray-100`,
