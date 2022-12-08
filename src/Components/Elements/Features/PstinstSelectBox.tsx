1ea2e095cf9916ad515562f86c10e358588eec59
psm <psm@healthmax.co.kr>
2022-12-08 12:08:17 +0900

    소속 선택 버그 수정.

--- src/Components/Elements/Features/PstinstSelectBox.tsx
+++ src/Components/Elements/Features/PstinstSelectBox.tsx
@@ -3,6 +3,7 @@ import { VarySelectBox } from '@Elements'
 import { getInstLowInstCode, getInstTopInstCode } from '@Service/CommonService'
 import { useMainLayouts } from '@Hook/index'
 import Messages from '@Messages'
+import { DefaultStatus } from '@CommonTypes'
 
 interface StepItemInfoInterface {
     INST_NO: number | null
@@ -17,6 +18,11 @@ interface StepItemInterface {
 }
 
 const initializeState = {
+    status: {
+        step1: 'idle',
+        step2: 'idle',
+        step3: 'idle',
+    },
     InstList: {
         step1: [],
         step2: [],
@@ -44,6 +50,8 @@ const PstinstSelectBox = ({
     ReturnCallback,
 }: {
     Value: {
+        status: DefaultStatus
+
         infoStep: string | 'step1' | 'step2' | 'step3'
         instNo: string | null
         step1: string
@@ -64,6 +72,11 @@ const PstinstSelectBox = ({
 }) => {
     const { handlMainAlert } = useMainLayouts()
     const [pageState, setPageState] = useState<{
+        status: {
+            step1: string | DefaultStatus
+            step2: string | DefaultStatus
+            step3: string | DefaultStatus
+        }
         InstList: {
             step1: StepItemInterface[]
             step2: StepItemInterface[]
@@ -83,6 +96,11 @@ const PstinstSelectBox = ({
         if (status) {
             setPageState(prevState => ({
                 ...prevState,
+                status: {
+                    step1: 'success',
+                    step2: 'idle',
+                    step3: 'idle',
+                },
                 InstList: {
                     ...prevState.InstList,
                     step1: payload.INST_CODE_LIST.map(e => {
@@ -98,7 +116,10 @@ const PstinstSelectBox = ({
         } else {
             setPageState(prevState => ({
                 ...prevState,
-                status: false,
+                status: {
+                    ...prevState.status,
+                    step1: 'failure',
+                },
             }))
             handlMainAlert({
                 state: true,
@@ -109,12 +130,25 @@ const PstinstSelectBox = ({
 
     const handleStep1Change = useCallback(
         async ({ value, text }: { value: string; text: string }) => {
+            setPageState(prevState => ({
+                ...prevState,
+                status: {
+                    ...prevState.status,
+                    step2: 'loading',
+                    step3: 'idle',
+                },
+            }))
+
             const { status, payload } = await getInstLowInstCode({
                 instNo: Number(value),
             })
             if (status) {
                 setPageState(prevState => ({
                     ...prevState,
+                    status: {
+                        ...prevState.status,
+                        step2: 'success',
+                    },
                     InstList: {
                         ...prevState.InstList,
                         step2: payload.INST_CODE_LIST.map(e => {
@@ -136,6 +170,13 @@ const PstinstSelectBox = ({
                     },
                 }))
             } else {
+                setPageState(prevState => ({
+                    ...prevState,
+                    status: {
+                        ...prevState.status,
+                        step2: 'failure',
+                    },
+                }))
                 handlMainAlert({
                     state: true,
                     message: Messages.Default.pageError,
@@ -158,12 +199,24 @@ const PstinstSelectBox = ({
                 },
             }))
 
+            setPageState(prevState => ({
+                ...prevState,
+                status: {
+                    ...prevState.status,
+                    step3: 'loading',
+                },
+            }))
+
             const { status, payload } = await getInstLowInstCode({
                 instNo: Number(value),
             })
             if (status) {
                 setPageState(prevState => ({
                     ...prevState,
+                    status: {
+                        ...prevState.status,
+                        step3: 'success',
+                    },
                     InstList: {
                         ...prevState.InstList,
                         step3: payload.INST_CODE_LIST.map(e => {
@@ -175,6 +228,13 @@ const PstinstSelectBox = ({
                     },
                 }))
             } else {
+                setPageState(prevState => ({
+                    ...prevState,
+                    status: {
+                        ...prevState.status,
+                        step3: 'failure',
+                    },
+                }))
                 handlMainAlert({
                     state: true,
                     message: Messages.Default.pageError,
@@ -204,7 +264,14 @@ const PstinstSelectBox = ({
     }
 
     useEffect(() => {
-        ReturnCallback(pageState.InstSelectList)
+        if (
+            Value.step1 !== pageState.InstSelectList.step1.value ||
+            Value.step2 !== pageState.InstSelectList.step2.value ||
+            Value.step3 !== pageState.InstSelectList.step3.value
+        ) {
+            ReturnCallback(pageState.InstSelectList)
+        }
+
         // FIXME : 종속성에서 ReturnCallback 업데이트 되면 무한 로딩이 걸려서 disable 리펙토링시에 수정 필요.
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [pageState.InstSelectList])
@@ -221,86 +288,104 @@ const PstinstSelectBox = ({
 
     useEffect(() => {
         const funcSetInst = () => {
-            const { infoStep, instNo, step1, step2 } = Value
+            if (Value) {
+                const { infoStep, instNo, step1, step2 } = Value
 
-            if (
-                instNo &&
-                infoStep === 'step1' &&
-                step1 !== pageState.InstSelectList.step1.value
-            ) {
-                handleStep1Change({ value: step1, text: '' }).then()
-                return
-            }
+                if (
+                    instNo &&
+                    infoStep === 'step1' &&
+                    step1 !== pageState.InstSelectList.step1.value
+                ) {
+                    handleStep1Change({ value: step1, text: '' }).then()
+                    return
+                }
 
-            if (
-                instNo &&
-                infoStep === 'step2' &&
-                step2 !== pageState.InstSelectList.step2.value
-            ) {
-                handleStep1Change({ value: step1, text: '' }).then(() =>
-                    handleStep2Change({
-                        value: instNo,
-                        text: '',
-                    }).then()
-                )
-
-                return
-            }
-
-            if (
-                instNo &&
-                infoStep === 'step3' &&
-                instNo !== pageState.InstSelectList.step3.value
-            ) {
-                handleStep1Change({ value: step1, text: '' }).then(() =>
-                    handleStep2Change({
-                        value: step2,
-                        text: '',
-                    }).then(() =>
-                        handleStep3Change({ value: instNo, text: '' })
+                if (
+                    instNo &&
+                    infoStep === 'step2' &&
+                    step2 !== pageState.InstSelectList.step2.value
+                ) {
+                    handleStep1Change({ value: step1, text: '' }).then(() =>
+                        handleStep2Change({
+                            value: instNo,
+                            text: '',
+                        }).then()
                     )
-                )
 
-                return
+                    return
+                }
+
+                if (
+                    instNo &&
+                    infoStep === 'step3' &&
+                    instNo !== pageState.InstSelectList.step3.value
+                ) {
+                    handleStep1Change({ value: step1, text: '' }).then(() =>
+                        handleStep2Change({
+                            value: step2,
+                            text: '',
+                        }).then(() =>
+                            handleStep3Change({ value: instNo, text: '' })
+                        )
+                    )
+
+                    return
+                }
             }
         }
 
-        if (Value.infoStep) {
+        if (
+            Value.status === 'success' &&
+            Value.infoStep &&
+            (Value.step1 !== pageState.InstSelectList.step1.value ||
+                Value.step2 !== pageState.InstSelectList.step2.value ||
+                Value.step3 !== pageState.InstSelectList.step3.value)
+        ) {
             funcSetInst()
         }
         // FIXME : 종속성에서 ReturnCallback 업데이트 되면 무한 로딩이 걸려서 disable 리펙토링시에 수정 필요.
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [Value])
 
+    // useEffect(() => {
+    //     console.debug('status', pageState.status)
+    //     console.debug('InstList', pageState.InstList)
+    // }, [pageState.status, pageState.InstList])
+
     return (
         <>
             <div className="flex flex-nowrap w-full">
                 <div className="min-w-1/4 pr-2">
-                    <VarySelectBox
-                        Placeholder={Messages.Default.instSelectStep1}
-                        Value={pageState.InstSelectList.step1.value}
-                        Elements={pageState.InstList.step1}
-                        HandleOnChange={e => handleStep1Change(e)}
-                    />
+                    {pageState.status.step1 === 'success' &&
+                        pageState.InstList.step1.length > 0 && (
+                            <VarySelectBox
+                                Placeholder={Messages.Default.instSelectStep1}
+                                Value={pageState.InstSelectList.step1.value}
+                                Elements={pageState.InstList.step1}
+                                HandleOnChange={e => handleStep1Change(e)}
+                            />
+                        )}
                 </div>
                 <div className="w-1/4 pr-2">
-                    {pageState.InstList.step2.length > 0 && (
-                        <VarySelectBox
-                            Placeholder={Messages.Default.instSelectStep2}
-                            Value={pageState.InstSelectList.step2.value}
-                            Elements={pageState.InstList.step2}
-                            HandleOnChange={e => handleStep2Change(e)}
-                        />
-                    )}
+                    {pageState.status.step2 === 'success' &&
+                        pageState.InstList.step2.length > 0 && (
+                            <VarySelectBox
+                                Placeholder={Messages.Default.instSelectStep2}
+                                Value={pageState.InstSelectList.step2.value}
+                                Elements={pageState.InstList.step2}
+                                HandleOnChange={e => handleStep2Change(e)}
+                            />
+                        )}
                 </div>
                 <div className="w-1/4 pr-2">
-                    {pageState.InstList.step3.length > 0 && (
-                        <VarySelectBox
-                            Value={pageState.InstSelectList.step3.value}
-                            Elements={pageState.InstList.step3}
-                            HandleOnChange={e => handleStep3Change(e)}
-                        />
-                    )}
+                    {pageState.status.step3 === 'success' &&
+                        pageState.InstList.step3.length > 0 && (
+                            <VarySelectBox
+                                Value={pageState.InstSelectList.step3.value}
+                                Elements={pageState.InstList.step3}
+                                HandleOnChange={e => handleStep3Change(e)}
+                            />
+                        )}
                 </div>
             </div>
         </>
