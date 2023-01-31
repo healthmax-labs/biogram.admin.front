54183f2c7ec1951a6e73b910ea17fdbbf236c5b9
psm <psm@healthmax.co.kr>
2023-01-31 16:45:55 +0900

    recoil 정리.

--- src/Recoil/MemberPagesState.ts
+++ src/Recoil/MemberPagesState.ts
@@ -10,8 +10,16 @@ import {
     MemberInfoInterface,
     MemberInfoListInterface,
 } from '@Type/MemberTypes'
-import { getNowDate, getOneMonthAgo, getOneYearAgo } from '@Helper'
+import {
+    getNowDate,
+    getOneMonthAgo,
+    getOneYearAgo,
+    getSearchDateObject,
+} from '@Helper'
 import Const from '@Const'
+import { MsgSendListInterface } from '@Type/MsgTypes'
+
+const searchDateObject = getSearchDateObject()
 
 // member 페이지.
 interface MemberListInterface {
@@ -132,6 +140,36 @@ interface ConsultMsgBoxListInterface {
         SEARCH_KEY: string
     }
     data: ManageCounselMsgBoxListInterface | null
+}
+
+//메세지 발송 현황
+interface MsgSendSearchListInterface {
+    status: DefaultStatus
+    search: {
+        curPage: number | null
+        INST_NO: string | null
+        SEARCH_KEY: string | null
+        FROM_MONTH: string
+        FROM_DAY: string
+        TO_DAY: string
+        SNDNG_FAILR: 'F' | null
+        SNDNG_STDR: string | null
+    }
+    list: MsgSendListInterface
+}
+
+//메세지 예약 현황
+interface MsgBookListInterface {
+    status: DefaultStatus
+    search: {
+        curPage: number | null
+        INST_NO: string | null
+        SEARCH_KEY: string | null
+        FROM_DAY: string | null
+        TO_DAY: string | null
+        SNDNG_STDR: string | null
+    }
+    list: MsgSendListInterface
 }
 
 // 회원 현황 리스트 페이지
@@ -334,5 +372,45 @@ export const ConsultMsgBoxListState = atom<ConsultMsgBoxListInterface>({
             SEARCH_KEY: '',
         },
         data: null,
+    },
+})
+
+export const MsgSendListState = atom<MsgSendSearchListInterface>({
+    key: `statusPage/msg-send-list`,
+    default: {
+        status: 'idle',
+        search: {
+            curPage: null,
+            INST_NO: null,
+            SEARCH_KEY: null,
+            SNDNG_FAILR: null,
+            SNDNG_STDR: null,
+            FROM_MONTH: `${searchDateObject.start.year}${searchDateObject.start.month}`,
+            FROM_DAY: `01`,
+            TO_DAY: `19`,
+        },
+        list: {
+            SMS_INFO_LIST: [],
+            TOTAL_COUNT: 0,
+        },
+    },
+})
+
+export const MsgBookListState = atom<MsgBookListInterface>({
+    key: `statusPage/msg-book-list`,
+    default: {
+        status: 'idle',
+        search: {
+            curPage: null,
+            INST_NO: null,
+            SEARCH_KEY: null,
+            FROM_DAY: getOneMonthAgo(),
+            TO_DAY: getNowDate(),
+            SNDNG_STDR: ``,
+        },
+        list: {
+            SMS_INFO_LIST: [],
+            TOTAL_COUNT: 0,
+        },
     },
 })
