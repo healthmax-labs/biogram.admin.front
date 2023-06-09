f207399c7fe43849af0dce19f0d3ff767035eef6
psm <psm@healthmax.co.kr>
2023-06-09 15:48:24 +0900

    * ip 정보 조회 변경.

--- src/Services/EtcService.ts
+++ src/Services/EtcService.ts
@@ -10,26 +10,32 @@ import { getVtokenInfoToken } from '@Helper'
 /**
  * ip 정보 가지고 오기.
  */
-export const getGeolocation = (): Promise<
-    ServicesDefaultResult<GeolocationDbInterface>
-> => {
-    return new Promise((resolve, reject) => {
-        axios
-            .get<GeolocationDbInterface>('https://jsonip.com/')
-            .then(res => {
-                resolve({
-                    status: true,
-                    message: '정상 전송하였습니다.',
-                    payload: res.data,
-                })
-            })
-            .catch(err =>
-                reject({
-                    status: false,
-                    message: err.message,
-                })
-            )
-    })
+export const getGeolocation = async (): Promise<{
+    status: boolean
+    message: string
+    payload: GeolocationDbInterface
+}> => {
+    try {
+        const response = await axios.get<GeolocationDbInterface>(
+            'https://jsonip.com/'
+        )
+        return {
+            status: true,
+            message: '정상 전송하였습니다.',
+            payload: response.data,
+        }
+    } catch (error) {
+        return {
+            status: false,
+            message: '에러가 발생했습니다.',
+            payload: {
+                ip: '',
+                country: '',
+                'geo-ip': '',
+                'API Help': '',
+            },
+        }
+    }
 }
 
 /**
